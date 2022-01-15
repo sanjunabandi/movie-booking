9bc1558aec9357e54af6876c3c226d842b6cf2c8
sanjunabandi <89703392+sanjunabandi@users.noreply.github.com>
2022-01-15 22:01:37 +0530

    Adding header changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import {Header} from "./common/header/Header"
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header/>
   </React.StrictMode>,
   document.getElementById('root')
 );
